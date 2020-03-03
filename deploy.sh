67a0675f5afc9ba24e4543c09810a4c395b20c18
Matt Banner <mattsbanner@gmail.com>
2020-03-03 17:51:20 +0000

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker-compose -f 'stacks/portainer/docker-compose.yml' -p 'portainer' up -d --remove-orphans
+docker-compose -f 'stacks/mysql/docker-compose.yml' -p 'mysql' up -d --remove-orphans
+docker-compose -f 'stacks/redis/docker-compose.yml' -p 'redis' up -d --remove-orphans
+docker-compose -f 'stacks/mailhog/docker-compose.yml' -p 'mailhog' up -d --remove-orphans
+docker-compose -f 'stacks/mongo/docker-compose.yml' -p 'mongo' up -d --remove-orphans
