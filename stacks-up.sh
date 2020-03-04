f819ff92b5d33f1de667a3b735d5c1bb2be7e794
Matt Banner <mattsbanner@gmail.com>
2020-03-04 18:56:34 +0000

    Add MySQL version and some todos

--- stacks-up.sh
+++ stacks-up.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# TODO: Check the .env file exists
+
+# TODO: For each of the values check it does not equal the placeholder (itself in %%)
+
 #docker-compose -f 'stacks/portainer/docker-compose.yml' -p 'portainer' up -d --remove-orphans
 #docker-compose -f 'stacks/mysql/docker-compose.yml' -p 'mysql' up -d --remove-orphans
 #docker-compose -f 'stacks/redis/docker-compose.yml' -p 'redis' up -d --remove-orphans
